0aebb2ee35cb83f04f43b47798941743c1f1596d
Faisal Almail <faisalyounis90@gmail.com>
2025-10-21 07:18:19 +0300

    express troubleshooting

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+
 const contactForm = document.getElementById("contact-form")
 
 
@@ -18,6 +20,18 @@ contactForm.addEventListener("submit", function (e){
     console.log(e.target.email.value)
     console.log(e.target.message.value)
 
+    const formData = {name: e.target.name.value,
+        email: e.target.email.value,
+        message: e.target.message.value
+    }
+
+    fetch("http://localhost:3000/save", { method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData)});
+    })
+/*     .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err)) */
+
     
 
-})
